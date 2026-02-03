63e194c106c671947c533a795f2d7e534fbd637a
Aniket Raj <araj52444@gmail.com>
2026-02-03 12:57:05 +0530

    add file reading and writing functionality to server

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 import http from "http";
+import fs from "fs";
 
 const PORT = 4000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from server ")
+
+    // fs.readFile("test.txt",(err,data)=>{
+    //     if (err) throw err;
+    //     const text = data.toString();
+    //     res.end(text);
+    // })
+
+    const data = fs.readFileSync("test.txt");
+    fs.writeFile("new.txt","this is new file.", (err)=>{
+        if (err) throw err;
+        console.log("File is created")
+    })
+
+    res.end(data.toString());
 });
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+//callback hell - nesting callbacks
+// method.(()=>{
+//     methode2.(()=>{
+//         method3.(()=>{
+            
+//         })
+//     })
+// })
