a3ef492fee500631bea5362f2da3e1cba28fa22a
Aniket Raj <araj52444@gmail.com>
2026-02-04 09:55:37 +0530

    Add user filtering and sorting functionality to /user endpoint

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 const users = [
   {
     id: 1,
-    name: "Aniket",
+    name: "Aditya",
     email: "aniket@gmail.com",
   },
   {
@@ -64,6 +64,41 @@ app.get("/users/:id", (req, res) => {
     });
   }
 });
+
+app.get("/user", (req,res)=>{
+  try {
+    const name = req.query.name;
+
+    const sort = req.query.sort ?? "asc"; // req.query.sort ? req.query.sort : "asc"
+
+
+    if(!name || typeof name !== "string"){
+        return res.status(400).json({
+            success: false,
+            message: "Name is required or Invalid"
+        })
+    }
+    const filteredUsers = users.filter((u)=>{
+      return u.name === name
+    })
+
+    const sorted = filteredUsers.sort((a,b)=>{
+      return (
+        sort === "asc" ? a.id - b.id : b.id - a.id
+      )
+    })
+    
+    res.status(200).json({
+        success: true,
+        data: sorted
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: `Something went wrong: ${error.message}`,
+    })
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
